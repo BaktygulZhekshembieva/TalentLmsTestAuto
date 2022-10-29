6ed570eea9f5dfb37002ca1838812bfd1e666b90
Baktygul <baktygul.jekshembieva.ch@mail.com>
2022-10-29 22:00:59 +0600

    My second commit4

--- src/main/java/git/gitt/Git2.java
+++ src/main/java/git/gitt/Git2.java
@@ -4,5 +4,6 @@ public class Git2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(100);
+        System.out.println(100);
     }
 }
